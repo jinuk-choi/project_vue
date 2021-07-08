465dfbcab5a5699a9b4dd215e9fee87f57a47bd2
jinuk-choi <81605147+jinuk-choi@users.noreply.github.com>
2021-07-08 10:32:40 +0900

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
+import ItemList from '@/components/admin/ItemList.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +12,21 @@ const routes = [
     name: 'Home',
     component: Home
   },
+
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    children: [
+      { 
+        path: 'ItemList',
+        name: ItemList,
+        component: ItemList
+      }
+
+    ]
+  },
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Admin.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+
+        <v-list-item router-link to="/admin/ItemList">
+          <v-list-item-action>
+            <v-icon>fas fa-search</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>재고관리</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item router :to="{name:'admin'}">
+          <v-list-item-action>
+            <v-icon>mdi-card-text-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>관리자</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+  
+        <v-list-item router :to="{name:'BoardList'}">
+          <v-list-item-action>
+            <v-icon>fas fa-list</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>게시판</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>ADMINISTRATOR</v-toolbar-title>
+      <v-btn right>로그아웃</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view/>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="primary"
+      app
+    >
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+
+
+export default {
+  name: 'Admin',
+
+  data: () => ({
+    //
+    drawer: true,
+  }),
+};
+</script>
