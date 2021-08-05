6a5e87410be29cbee5cce868a611a0b745653381
jinuk-choi <81605147+jinuk-choi@users.noreply.github.com>
2021-08-05 16:27:54 +0900

    8.05 kakao_2

--- src/views/Login/klogin.vue
+++ src/views/Login/klogin.vue
@@ -1,29 +1,110 @@
 <template>
-    <h2>환영합니다</h2>   
+  <div class="klogin">
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="card">
+        <div class="row">
+          <div class="col-md-6">
+            <b-form
+              class="form"
+              @submit="onSubmit"
+              @reset="onReset"
+              v-if="show"
+            >
+              <h2>회원가입</h2>
+              <div class="inputbox mt-3">
+                <span>이름</span>
+                <input
+                  type="text"
+                  placeholder="실명을 입력해주세요"
+                  class="form-control"
+                  v-model="form.uname"
+                  required
+                />
+              </div>
+              <div class="inputbox mt-3">
+                <span>이메일</span>
+                <input
+                  type="text"
+                  v-text="form.email"
+                  v-model="form.email"
+                  class="form-control"
+                  required
+                  disabled
+                />
+              </div>
+              <div class="inputbox mt-3">
+                <span>휴대폰 번호</span>
+                <input
+                  type="text"
+                  placeholder="010-0000-0000"
+                  name=""
+                  class="form-control"
+                />
+              </div>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="text-right">
+                  <button
+                    type="submit"
+                    class="btn btn-sucess register btn-block"
+                  >
+                  Register
+                  </button>
+                </div>
+                <div class="text-right">
+                  <button
+                    type="reset"
+                    class="btn btn-sucess reset btn-block"
+                  >
+                  Reset
+                  </button>
+                </div>
+              </div>
+            </b-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import axios from 'axios'
 import Route from '@/router/index'
 
 export default {
-  data() {
-    return {
-      codes:""    
-    }
-  },
   created() {
       this.create();
   },
-
+  data() {
+    return {
+      codes:"" ,   
+      form: {
+        password: "",
+        email: "",
+        uname: "",
+      },
+      show: true,
+    };
+  },
   methods: {
     create() {
         this.codes = this.$route.query.code;
-        this.klogin();
+        this.getToken();
     },
-    
-
-
-    klogin() {
+    login() {
+      var dns = this.$store.state.dns
+        return new Promise((resolve, reject) => {
+            axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
+                .then(Response => {
+                    console.log(Response.data)
+                })
+                .catch(Error => {
+                    console.log('error')
+                    reject(Error)
+                })
+        })
+     
+    },
+    getToken() {
         var dns = this.$store.state.dns
         return new Promise((resolve, reject) => {
             axios.post('http://'+ dns +'/klogin?authorize_code=' + this.codes)
@@ -36,7 +117,17 @@ export default {
                     reject(Error)
                 })
         })
-        },
+    },
+    onSubmit(event) {
+      event.preventDefault();
+      axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
+        .then(Response => {
+            console.log(Response.data)                 
+        })
+    }
+
+
+
   }
 
 }
