9148926311d7a6eeae8835e45f68650c58d6b659
jinuk-choi <81605147+jinuk-choi@users.noreply.github.com>
2021-08-06 11:52:30 +0900

    8.06 kakao jwt

--- src/views/Login/klogin.vue
+++ src/views/Login/klogin.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="klogin">
-    <div class="container d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-center align-items-center; ">
       <div class="card">
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-md-12">
             <b-form
               class="form"
-              @submit="onSubmit"
-              @reset="onReset"
               v-if="show"
             >
               <h2>회원가입</h2>
@@ -17,7 +15,7 @@
                   type="text"
                   placeholder="실명을 입력해주세요"
                   class="form-control"
-                  v-model="form.uname"
+                  v-model="name"
                   required
                 />
               </div>
@@ -25,8 +23,8 @@
                 <span>이메일</span>
                 <input
                   type="text"
-                  v-text="form.email"
-                  v-model="form.email"
+                  v-text="username"
+                  v-model="username"
                   class="form-control"
                   required
                   disabled
@@ -37,26 +35,15 @@
                 <input
                   type="text"
                   placeholder="010-0000-0000"
-                  name=""
+                  v-model="phone"
                   class="form-control"
                 />
               </div>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="text-right">
-                  <button
-                    type="submit"
-                    class="btn btn-sucess register btn-block"
-                  >
-                  Register
-                  </button>
-                </div>
-                <div class="text-right">
-                  <button
-                    type="reset"
-                    class="btn btn-sucess reset btn-block"
-                  >
-                  Reset
-                  </button>
+                 <v-btn type="submit" width="300px" height="50px" 
+                  @click="Join({username, name, phone, password})">등록하기
+                 </v-btn>
                 </div>
               </div>
             </b-form>
@@ -69,6 +56,7 @@
 <script>
 import axios from 'axios'
 import Route from '@/router/index'
+import {mapActions} from "vuex"
 
 export default {
   created() {
@@ -77,40 +65,37 @@ export default {
   data() {
     return {
       codes:"" ,   
-      form: {
-        password: "",
-        email: "",
-        uname: "",
-      },
+      phone: "",
+      username: "",
+      name: "",
+      password:" ",
+      jwt:"",
       show: true,
     };
   },
   methods: {
+    ...mapActions(["Join"]),
+    ...mapActions(["Login"]),
+
     create() {
         this.codes = this.$route.query.code;
         this.getToken();
     },
-    login() {
-      var dns = this.$store.state.dns
-        return new Promise((resolve, reject) => {
-            axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
-                .then(Response => {
-                    console.log(Response.data)
-                })
-                .catch(Error => {
-                    console.log('error')
-                    reject(Error)
-                })
-        })
-     
-    },
+  
     getToken() {
         var dns = this.$store.state.dns
         return new Promise((resolve, reject) => {
             axios.post('http://'+ dns +'/klogin?authorize_code=' + this.codes)
                 .then(Response => {
                     console.log(Response.data)
-                    Route.push("/Main")
+                    this.username = Response.data.email
+                    this.password = Response.data.nickname
+                    this.jwt = this.codes
+                    if (this.password == undefined) {
+                    } else {
+                      this.Login({username:this.username, password:this.password, jwt:this.jwt})
+                    }
+                  
                 })
                 .catch(Error => {
                     console.log('error')
@@ -118,13 +103,30 @@ export default {
                 })
         })
     },
-    onSubmit(event) {
-      event.preventDefault();
-      axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
-        .then(Response => {
-            console.log(Response.data)                 
-        })
-    }
+
+
+
+    // onSubmit(event) {
+    //   event.preventDefault();
+    //   axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
+    //     .then(Response => {
+    //         console.log(Response.data)                 
+    //     })
+    // }
+    //   login() {
+    //   var dns = this.$store.state.dns
+    //     return new Promise((resolve, reject) => {
+    //         axios.post('http://'+ dns +'/klogin?authorize_code=' + this.form)
+    //             .then(Response => {
+    //                 console.log(Response.data)
+    //             })
+    //             .catch(Error => {
+    //                 console.log('error')
+    //                 reject(Error)
+    //             })
+    //     })
+     
+    // },
 
 
 
